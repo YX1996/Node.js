cdeb43502f330e5f7d0ee7535403d3a2e96776df
王悦鑫 <wangyuexindy@qq.com>
2017-09-04 17:55:42 +0800

    UPD客户端

---
+++ net dgram/updClient.js
@@ -0,0 +1,9 @@
+var dgram = require('dgram');
+
+var client = dgram.createSocket('udp4');
+
+var message = new Buffer('hello shiyanlou');
+
+client.send(message, 0, message.length, 41234, 'localhost', function(err, bytes) {
+    client.close();
+});
